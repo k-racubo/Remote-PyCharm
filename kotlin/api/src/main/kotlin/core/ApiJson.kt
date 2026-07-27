22b2aab03956bae9766168f196cadeb36ea43239
Nikitos <pzdankov@gmail.com>
2026-07-27 21:53:34 +0300

    [api] fix: removed duplicate subclasses in serializersModule

--- kotlin/api/src/main/kotlin/core/ApiJson.kt
+++ kotlin/api/src/main/kotlin/core/ApiJson.kt
@@ -28,11 +28,9 @@ object ApiJson {
         serializersModule = SerializersModule {
             polymorphic(Command::class) {
                 subclass(GetProjectsList::class)
-                subclass(OpenProjectCommand::class)
                 subclass(RunCurrentConfigCommand::class)
                 subclass(GetFileContent::class)
                 subclass(OpenProjectCommand::class)
-                subclass(RunCurrentConfigCommand::class)
                 subclass(CloseProjectCommand::class)
                 subclass(StopCurrentConfigCommand::class)
             }
@@ -43,8 +41,6 @@ object ApiJson {
                 subclass(ResultOfRunResponse::class)
                 subclass(ErrorResponse::class)
                 subclass(FileContentResponse::class)
-                subclass(ProjectFileTreeResponse::class)
-                subclass(ResultOfRunResponse::class)
             }
 
             polymorphic(Event::class) {
